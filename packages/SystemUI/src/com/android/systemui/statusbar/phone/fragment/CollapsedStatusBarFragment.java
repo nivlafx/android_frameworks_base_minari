9ac09f29d2284ccc83de610374a6a4371e448266
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-19 07:20:58 +0800

    SystemUI: Hide status bar logo when headsup is visible
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -602,7 +602,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         boolean showOngoingCallChip = mOngoingCallController.hasOngoingCall() && !headsUpVisible;
         return new StatusBarVisibilityModel(
                 showClock,
-                externalModel.getShowNotificationIcons(),
+                externalModel.getShowNotificationIcons() && !headsUpVisible,
                 showOngoingCallChip,
                 externalModel.getShowSystemInfo());
     }
@@ -615,9 +615,11 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         StatusBarVisibilityModel visibilityModel = mLastModifiedVisibility;
         boolean disableNotifications = !visibilityModel.getShowNotificationIcons();
         boolean hasOngoingCall = visibilityModel.getShowOngoingCallChip();
+        boolean headsUpVisible =
+                mStatusBarFragmentComponent.getHeadsUpAppearanceController().shouldBeVisible();
 
         // Hide notifications if the disable flag is set or we have an ongoing call.
-        if (disableNotifications || hasOngoingCall) {
+        if (disableNotifications || hasOngoingCall || headsUpVisible) {
             hideNotificationIconArea(animate);
         } else {
             showNotificationIconArea(animate);
