e0bf62d9d6616a3609780aba10abe027e9f4da24
Mateus Azis <azis@google.com>
2023-12-28 11:05:22 -0800

    Remove unused reference to Installer.
    This reference is never used and holding it for longer than needed could
    hurt garbage collection.
    
    Bug: 274764512
    Test: built and ran a local cuttlefish instance
    Change-Id: I81d44c95bc7ef83ad525072122f0275d9df9975e
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1169,7 +1169,6 @@ public final class SystemServer implements Dumpable {
         mActivityManagerService = ActivityManagerService.Lifecycle.startService(
                 mSystemServiceManager, atm);
         mActivityManagerService.setSystemServiceManager(mSystemServiceManager);
-        mActivityManagerService.setInstaller(installer);
         mWindowManagerGlobalLock = atm.getGlobalLock();
         t.traceEnd();
 
