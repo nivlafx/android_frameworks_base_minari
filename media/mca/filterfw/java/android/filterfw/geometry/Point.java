alpha4041/android_frameworks_base

media: Cache sin/cos values
* recoomended by: Marius Renn <renn@google.com>

Change-Id: I0cc9210512d1626fd02dce3c7074e3e801b63494
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

media/mca/filterfw/java/android/filterfw/geometry/Point.java
@@ -19,11 +19,17 @@ package android.filterfw.geometry;
 
 import android.compat.annotation.UnsupportedAppUsage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @hide
  */
 public class Point {
 
+    private static final Map<Float, Float> cosCache = new HashMap<>();
+    private static final Map<Float, Float> sinCache = new HashMap<>();
+    
     @UnsupportedAppUsage
     public float x;
     @UnsupportedAppUsage
@@ -101,9 +107,23 @@ public class Point {
     }
 
     public Point rotated(float radians) {
-        // TODO(renn): Optimize: Keep cache of cos/sin values
-        return new Point((float)(Math.cos(radians) * x - Math.sin(radians) * y),
-                         (float)(Math.sin(radians) * x + Math.cos(radians) * y));
+        float cosVal, sinVal;
+
+        if (cosCache.containsKey(radians)) {
+            cosVal = cosCache.get(radians);
+        } else {
+            cosVal = (float) Math.cos(radians);
+            cosCache.put(radians, cosVal);
+        }
+
+        if (sinCache.containsKey(radians)) {
+            sinVal = sinCache.get(radians);
+        } else {
+            sinVal = (float) Math.sin(radians);
+            sinCache.put(radians, sinVal);
+        }
+
+        return new Point(cosVal * x - sinVal * y, sinVal * x + cosVal * y);
     }
 
     public Point rotatedAround(Point center, float radians) {

nivlafx/android_frameworks_base_minari

media/mca/filterfw/java/android/filterfw/geometry/Point.java
@@ -19,11 +19,17 @@ package android.filterfw.geometry;
 
 import android.compat.annotation.UnsupportedAppUsage;
 
+import android.util.SparseArray;
+import java.util.Map;
+
 /**
  * @hide
  */
 public class Point {
 
+    private static final SparseArray<Float> cosCache = new SparseArray<>();
+    private static final SparseArray<Float> sinCache = new SparseArray<>();
+    
     @UnsupportedAppUsage
     public float x;
     @UnsupportedAppUsage
@@ -101,9 +107,28 @@ public class Point {
     }
 
     public Point rotated(float radians) {
-        // TODO(renn): Optimize: Keep cache of cos/sin values
-        return new Point((float)(Math.cos(radians) * x - Math.sin(radians) * y),
-                         (float)(Math.sin(radians) * x + Math.cos(radians) * y));
+        float cosVal, sinVal;
+        int key = toSparseArrayKey(radians);
+
+        if (cosCache.get(key) != null) {
+            cosVal = cosCache.get(key);
+        } else {
+            cosVal = (float) Math.cos(radians);
+            cosCache.put(key, cosVal);
+        }
+
+        if (sinCache.get(key) != null) {
+            sinVal = sinCache.get(key);
+        } else {
+            sinVal = (float) Math.sin(radians);
+            sinCache.put(key, sinVal);
+        }
+
+        return new Point(cosVal * x - sinVal * y, sinVal * x + cosVal * y);
+    }
+
+    private int toSparseArrayKey(float radians) {
+        return (int)(radians * 10000);
     }
 
     public Point rotatedAround(Point center, float radians) {