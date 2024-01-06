davigamer987/android_frameworks_base

core: Implement system vibration haptics settings
* insipired from oplus haptics, based from aosp vibration effecs handling

Co-authored-by: Blaster4385 <venkateshchaturvedi12@gmail.com>
Change-Id: I9d928a902b5ae57073cb5893ac84ab96030ef9e3
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

core/java/android/widget/EdgeEffect.java
@@ -26,6 +26,7 @@ import android.compat.annotation.ChangeId;
 import android.compat.annotation.EnabledSince;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.TypedArray;
 import android.graphics.BlendMode;
 import android.graphics.Canvas;
@@ -34,7 +35,11 @@ import android.graphics.Paint;
 import android.graphics.RecordingCanvas;
 import android.graphics.Rect;
 import android.graphics.RenderNode;
+import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Vibrator;
+import android.os.VibrationEffect;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
@@ -217,6 +222,10 @@ public class EdgeEffect {
     private float mBaseGlowScale;
     private float mDisplacement = 0.5f;
     private float mTargetDisplacement = 0.5f;
+    
+    private boolean callerHasVibratePermission;
+    private Vibrator vibrator;
+    private int vibrateIntensity;
 
     /**
      * Current edge effect type, consumers should always query
@@ -233,6 +242,11 @@ public class EdgeEffect {
      */
     public EdgeEffect(Context context) {
         this(context, null);
+        callerHasVibratePermission = context.checkCallingOrSelfPermission(
+                    android.Manifest.permission.VIBRATE) == PackageManager.PERMISSION_GRANTED;
+        vibrator = context.getSystemService(Vibrator.class);
+        vibrateIntensity = Settings.System.getInt(context.getContentResolver(),
+                Settings.System.EDGE_SCROLLING_HAPTICS_INTENSITY, 3);
     }
 
     /**
@@ -253,6 +267,11 @@ public class EdgeEffect {
         mPaint.setColor((themeColor & 0xffffff) | 0x33000000);
         mPaint.setStyle(Paint.Style.FILL);
         mPaint.setBlendMode(DEFAULT_BLEND_MODE);
+        callerHasVibratePermission = context.checkCallingOrSelfPermission(
+                    android.Manifest.permission.VIBRATE) == PackageManager.PERMISSION_GRANTED;
+        vibrator = context.getSystemService(Vibrator.class);
+        vibrateIntensity = Settings.System.getInt(context.getContentResolver(),
+                Settings.System.EDGE_SCROLLING_HAPTICS_INTENSITY, 3);
     }
 
     @EdgeEffectType
@@ -262,6 +281,36 @@ public class EdgeEffect {
         } else {
             return mEdgeEffectType;
         }
+    }
+
+    private void triggerVibration() {
+        if (vibrator == null || !callerHasVibratePermission || vibrateIntensity == 0) {
+            return;
+        }
+
+            VibrationEffect effect;
+            switch (vibrateIntensity) {
+                case 1:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_TEXTURE_TICK);
+                    break;
+                case 2:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_TICK);
+                    break;
+                case 3:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK);
+                    break;
+                case 4:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_DOUBLE_CLICK);
+                    break;
+                case 5:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK);
+                    break;
+                default:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK);
+                    break;
+            }
+
+        AsyncTask.execute(() -> vibrator.vibrate(effect));
     }
 
     /**
@@ -497,6 +546,7 @@ public class EdgeEffect {
             mState = STATE_RECEDE;
             mVelocity = velocity * ON_ABSORB_VELOCITY_ADJUSTMENT;
             mStartTime = AnimationUtils.currentAnimationTimeMillis();
+            triggerVibration();
         } else if (edgeEffectBehavior == TYPE_GLOW) {
             mState = STATE_ABSORB;
             mVelocity = 0;
@@ -521,6 +571,7 @@ public class EdgeEffect {
                     mGlowAlphaStart,
                     Math.min(velocity * VELOCITY_GLOW_FACTOR * .00001f, MAX_ALPHA));
             mTargetDisplacement = 0.5f;
+            triggerVibration();
         } else {
             finish();
         }

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -73,6 +73,7 @@ import android.media.AudioSystem;
 import android.media.session.MediaController;
 import android.media.session.MediaSessionManager;
 import android.media.session.PlaybackState;
+import android.os.AsyncTask;
 import android.os.Debug;
 import android.os.Handler;
 import android.os.Looper;
@@ -2795,6 +2796,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                             userLevel);
                 }
             }
+            triggerVibration();
         }
 
         @Override
@@ -2816,6 +2818,31 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                         USER_ATTEMPT_GRACE_PERIOD);
             }
         }
+    }
+
+    private void triggerVibration() {
+        int vibrateIntensity = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.VOLUME_SLIDER_HAPTICS_INTENSITY, 1);
+        if (mController == null || vibrateIntensity == 0) {
+            return;
+        }
+            VibrationEffect effect;
+            switch (vibrateIntensity) {
+                case 1:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_TEXTURE_TICK);
+                    break;
+                case 2:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_TICK);
+                    break;
+                case 3:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK);
+                    break;
+                default:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_TEXTURE_TICK);
+                    break;
+            }
+
+        AsyncTask.execute(() -> mController.vibrate(effect));
     }
 
     private final class Accessibility extends AccessibilityDelegate {

nivlafx/android_frameworks_base_minari

core/java/android/widget/EdgeEffect.java
@@ -26,6 +26,7 @@ import android.compat.annotation.ChangeId;
 import android.compat.annotation.EnabledSince;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.TypedArray;
 import android.graphics.BlendMode;
 import android.graphics.Canvas;
@@ -34,7 +35,11 @@ import android.graphics.Paint;
 import android.graphics.RecordingCanvas;
 import android.graphics.Rect;
 import android.graphics.RenderNode;
+import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Vibrator;
+import android.os.VibrationEffect;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
@@ -217,6 +222,10 @@ public class EdgeEffect {
     private float mBaseGlowScale;
     private float mDisplacement = 0.5f;
     private float mTargetDisplacement = 0.5f;
+    
+    private boolean callerHasVibratePermission;
+    private Vibrator vibrator;
+    private int vibrateIntensity;
 
     /**
      * Current edge effect type, consumers should always query
@@ -233,6 +242,11 @@ public class EdgeEffect {
      */
     public EdgeEffect(Context context) {
         this(context, null);
+        callerHasVibratePermission = context.checkCallingOrSelfPermission(
+                    android.Manifest.permission.VIBRATE) == PackageManager.PERMISSION_GRANTED;
+        vibrator = context.getSystemService(Vibrator.class);
+        vibrateIntensity = Settings.Secure.getInt(context.getContentResolver(),
+                Settings.Secure.EDGE_SCROLLING_HAPTICS_INTENSITY, 3);
     }
 
     /**
@@ -253,6 +267,11 @@ public class EdgeEffect {
         mPaint.setColor((themeColor & 0xffffff) | 0x33000000);
         mPaint.setStyle(Paint.Style.FILL);
         mPaint.setBlendMode(DEFAULT_BLEND_MODE);
+        callerHasVibratePermission = context.checkCallingOrSelfPermission(
+                    android.Manifest.permission.VIBRATE) == PackageManager.PERMISSION_GRANTED;
+        vibrator = context.getSystemService(Vibrator.class);
+        vibrateIntensity = Settings.Secure.getInt(context.getContentResolver(),
+                Settings.Secure.EDGE_SCROLLING_HAPTICS_INTENSITY, 3);
     }
 
     @EdgeEffectType
@@ -262,6 +281,36 @@ public class EdgeEffect {
         } else {
             return mEdgeEffectType;
         }
+    }
+
+    private void triggerVibration() {
+        if (vibrator == null || !callerHasVibratePermission || vibrateIntensity == 0) {
+            return;
+        }
+
+            VibrationEffect effect;
+            switch (vibrateIntensity) {
+                case 1:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_TEXTURE_TICK);
+                    break;
+                case 2:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_TICK);
+                    break;
+                case 3:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK);
+                    break;
+                case 4:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_DOUBLE_CLICK);
+                    break;
+                case 5:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK);
+                    break;
+                default:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK);
+                    break;
+            }
+
+        AsyncTask.execute(() -> vibrator.vibrate(effect));
     }
 
     /**
@@ -497,6 +546,7 @@ public class EdgeEffect {
             mState = STATE_RECEDE;
             mVelocity = velocity * ON_ABSORB_VELOCITY_ADJUSTMENT;
             mStartTime = AnimationUtils.currentAnimationTimeMillis();
+            triggerVibration();
         } else if (edgeEffectBehavior == TYPE_GLOW) {
             mState = STATE_ABSORB;
             mVelocity = 0;
@@ -521,6 +571,7 @@ public class EdgeEffect {
                     mGlowAlphaStart,
                     Math.min(velocity * VELOCITY_GLOW_FACTOR * .00001f, MAX_ALPHA));
             mTargetDisplacement = 0.5f;
+            triggerVibration();
         } else {
             finish();
         }

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -80,6 +80,7 @@ import android.media.MediaPlayer;
 import android.media.session.MediaController;
 import android.media.session.MediaSessionManager;
 import android.media.session.PlaybackState;
+import android.os.AsyncTask;
 import android.os.Debug;
 import android.os.Handler;
 import android.os.Looper;
@@ -3070,7 +3071,10 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                             userLevel);
                 }
             }
-            playSound();
+            mHandler.post(() -> {
+                playSound();
+                triggerVibration();
+            });
         }
 
         @Override
@@ -3113,6 +3117,31 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         } catch (Exception e) {
             Log.d(TAG, "Error playing sound effect");
         }
+    }
+
+    private void triggerVibration() {
+        int vibrateIntensity = Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.VOLUME_SLIDER_HAPTICS_INTENSITY, 1);
+        if (mController == null || vibrateIntensity == 0) {
+            return;
+        }
+            VibrationEffect effect;
+            switch (vibrateIntensity) {
+                case 1:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_TEXTURE_TICK);
+                    break;
+                case 2:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_TICK);
+                    break;
+                case 3:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK);
+                    break;
+                default:
+                    effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_TEXTURE_TICK);
+                    break;
+            }
+
+        AsyncTask.execute(() -> mController.vibrate(effect));
     }
 
     private final class Accessibility extends AccessibilityDelegate {